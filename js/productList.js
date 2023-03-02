b40a268b5e07870296abf44e9f0ea9d8f3f7e800
ESPR07 <111272036+ESPR07@users.noreply.github.com>
2023-03-02 11:16:30 +0100

    Dynamic Details

--- js/productList.js
+++ js/productList.js
@@ -1,6 +1,6 @@
 export const productList = [
     {
-        "id": 1,
+        "id": "1",
         "name": "1st V2 Alpint Jacket",
         "color": "Orange/Grey",
         "description": 'Robust and durable 3-layer alpint jacket in wind and waterproof material. 1st V2 Alpint Jacket gives you good weather protection in challenging weather. The durable 3-layer Dermalax material has good breathing properties and is wind and waterproof.',
@@ -8,7 +8,7 @@ export const productList = [
         "image": "/images/RainyDays_Jacket1.webp"
     },
     {
-        "id": 2,
+        "id": "2",
         "name": "XM Xtreme Jacket",
         "color": "Grey",
         "description": "Functional every day jacket for all weathers. Lightweight and comfortable.",
@@ -16,7 +16,7 @@ export const productList = [
         "image": "/images/RainyDays_Jacket5.webp"
     },
     {
-        "id": 3,
+        "id": "3",
         "name": "Patagom Fleece Jacket",
         "color": "Brown",
         "description": "Wonderful fleece jacket for both wearing on it's own or under a jacket when the temperature get's low.",
@@ -24,7 +24,7 @@ export const productList = [
         "image": "/images/RainyDays_Jacket7.webp"
     },
     {
-        "id": 4,
+        "id": "4",
         "name": "Scott Bike Jacket",
         "color": "Black",
         "description": "Perfect jacket for you next biketrip, it's both durable and waterproof as well as windproof.",
@@ -32,7 +32,7 @@ export const productList = [
         "image": "/images/RainyDays_Jacket6.webp"
     },
     {
-        "id": 5,
+        "id": "5",
         "name": "Scott Wind Jacket",
         "color": "Grey",
         "description": "Windproof and waterproof wind jacket perfect for hiking in the mountain on a windy day.",
@@ -40,7 +40,7 @@ export const productList = [
         "image": "/images/RainyDays_Jacket2.webp"
     },
     {
-        "id": 6,
+        "id": "6",
         "name": "Comfrt Excercise Jacket",
         "color": "Red",
         "description": "Comfortable breathing jacket for outdoor excercises. It also has built in handwarmers.",
--- js/productPage.js
+++ js/productPage.js
@@ -1,9 +1,46 @@
 import { productList } from "./productList.js";
+const container = document.querySelector(".product-wrapper");
 
 const fetchID = document.location.search;
 const param = new URLSearchParams(fetchID);
 const ID = param.get("id");
 
-let product = productList.find(id => ID);
+const product = productList.find(product => product.id === ID);
 
 console.log(product);
+
+const imageContainer = document.createElement("div");
+imageContainer.classList.add("product-page-image-container");
+container.appendChild(imageContainer);
+
+const image = document.createElement("img");
+image.src = product.image;
+image.alt = product.name;
+imageContainer.appendChild(image);
+
+const contentContainer = document.createElement("section");
+contentContainer.classList.add("product-text-container");
+container.appendChild(contentContainer);
+
+const header = document.createElement("h1");
+header.innerText = product.name;
+contentContainer.appendChild(header);
+
+const content = document.createElement("p");
+content.innerText = product.description;
+contentContainer.appendChild(content);
+
+const buttonWrap = document.createElement("div");
+buttonWrap.classList.add("price-button-wrap");
+contentContainer.appendChild(buttonWrap);
+
+const price = document.createElement("h2");
+price.innerText = "kr " + product.price;
+buttonWrap.appendChild(price);
+
+const buyButton = document.createElement("a");
+buyButton.href = "/cart.html";
+buyButton.title = "Buy the product";
+buyButton.classList.add("product-page-buy-button");
+buyButton.innerText = "Buy Now"
+buttonWrap.appendChild(buyButton);
